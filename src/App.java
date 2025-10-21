7952cd7e87d2c398c62eb462924752c952d2ae0b
bittinikkari1 <groytopia@gmail.com>
2025-10-21 09:09:52 +0300

    Added some basic functionality in the form of printing different predetermined variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String tekija = "Pyry";
+        double luku1 = 3;
+        double luku2 = 8;
+
+        System.out.println("Hei olen tulostin-ohjelma");
+        System.out.println("Ohjelman tekijä: " + tekija);
+        System.out.println("Luku1-muuttujan arvo on " + luku1);
+        System.out.println("Luku1-muuttujan arvo on " + luku2);
+
+        
+
     }
 }
