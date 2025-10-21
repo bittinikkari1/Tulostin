32940748837e81a880e47f304655a30185486efc
bittinikkari1 <groytopia@gmail.com>
2025-10-21 09:19:10 +0300

    Calculations added to the printer

--- src/App.java
+++ src/App.java
@@ -1,15 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         String tekija = "Pyry";
-        double luku1 = 3;
-        double luku2 = 8;
+        double luku1 = 2;
+        double luku2 = 4;
+        double tulo = luku1 * luku2;
+        double erotus = luku1 - luku2;
+        double summa = luku1 + luku2;
+        double jako = luku1 / luku2;
 
         System.out.println("Hei olen tulostin-ohjelma");
         System.out.println("Ohjelman tekijä: " + tekija);
         System.out.println("Luku1-muuttujan arvo on " + luku1);
         System.out.println("Luku1-muuttujan arvo on " + luku2);
-
-        
-
-    }
+        System.out.println(luku1 + " * " + luku2 + " = " + tulo);
+        System.out.println(luku1 + " - " + luku2 + " = " + erotus);
+        System.out.println(luku1 + " + " + luku2 + " = " + summa);
+        System.out.println(luku1 + " / " + luku2 + " = " + jako);
+    }   
 }
